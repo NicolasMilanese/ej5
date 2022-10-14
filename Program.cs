4693ddcbfc89a35a147d3ef4298da0b7e5d12a25
NicolasMilanese <nicolasmilanese2004@gmail.com>
2022-10-13 21:24:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ejercicio
+{
+class Program
+{
+static void Main (string[] args)
+{
+Console.Clear();
+
+int num1,num2;
+string numero1,numero2;
+
+Console.WriteLine("Ingrese numero");
+numero1= Console.ReadLine();
+num1 = Convert.ToInt32 (numero1);
+Console.WriteLine("Ingrese numero");
+numero2 = Console.ReadLine();
+num2 = Convert.ToInt32 (numero2);
+
+if (num1>num2)
+{
+Console.WriteLine("El primer numero es mayor al segundo numero");
+}
+else
+{
+Console.WriteLine("El primer numero es menor al segundo numero");
+}
+}
+}
+}
